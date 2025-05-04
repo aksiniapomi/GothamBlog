9437d1da9be6ee4bf796dd7b807d75f28bab4a15
aksiniapomi <kseniapominova@gmail.com>
2025-05-04 22:03:15 +0100

    Final commit

--- backend/BloggingPlatformAPI/obj/Debug/net9.0/BloggingPlatformAPI.AssemblyInfo.cs
+++ backend/BloggingPlatformAPI/obj/Debug/net9.0/BloggingPlatformAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ed06a11af55aa03dd2faf6aa8618acf4d257ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b757f40526f38711d971e5c192f75c84a7fc8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloggingPlatformAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
